754775a7efaa94d971b3fcea0b3fcce8d1446cf4
daybreaker48 <prof.dagian@gmail.com>
2019-10-24 17:43:15 +0900

    --- editor 작업 중

--- app/src/main/java/com/mhd/boomerang/fragment/PostFragment.java
+++ app/src/main/java/com/mhd/boomerang/fragment/PostFragment.java
@@ -1,17 +1,33 @@
 package com.mhd.boomerang.fragment;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.mhd.boomerang.MainActivity;
 import com.mhd.boomerang.R;
+import com.mhd.boomerang.activity.TutorialActivity;
+import com.mhd.boomerang.common.MHDApplication;
+import com.mhd.boomerang.network.MHDNetworkInvoker;
+import com.mhd.boomerang.util.MHDDialogUtil;
+import com.mhd.boomerang.util.MHDLog;
 import com.mhd.boomerang.util.Util;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class PostFragment extends BaseFragment {
+
+    private Button btnSave;
+    private com.github.irshulx.Editor editor;
 
     public static PostFragment create() {
         return new PostFragment();
@@ -24,10 +40,76 @@ public class PostFragment extends BaseFragment {
 
     @Override
     public void inOnCreateView(View root, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //Statusbar 아래로 내리기
-        final TextView mTitle = (TextView) root.findViewById(R.id.vst_top_title);
-        RelativeLayout.LayoutParams mLayoutParams = (RelativeLayout.LayoutParams) mTitle.getLayoutParams();
-        mLayoutParams.topMargin = Util.getInstance().getStatusBarHeight(root.getContext());
-        mTitle.setLayoutParams(mLayoutParams);
+//        //Statusbar 아래로 내리기
+//        final TextView mTitle = (TextView) root.findViewById(R.id.vst_top_title);
+//        RelativeLayout.LayoutParams mLayoutParams = (RelativeLayout.LayoutParams) mTitle.getLayoutParams();
+//        mLayoutParams.topMargin = Util.getInstance().getStatusBarHeight(root.getContext());
+//        mTitle.setLayoutParams(mLayoutParams);
+
+//        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN | WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        ScrollView scrollView = (ScrollView) root.findViewById(R.id.svEditor);
+        btnSave = (Button) root.findViewById(R.id.btn_save_post);
+        editor = (com.github.irshulx.Editor) root.findViewById(R.id.editor);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                savePost();
+            }
+        });
+
+        scrollView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
+    }
+
+    private void savePost(){
+        //입력된 Text(우선)를 취합해서 서버 전송
+
+        //필터링. (gender, year, agree terms)
+        String postText = editor.getContentAsHTML();
+        if(postText.length() == 0){ MHDDialogUtil.sAlert(mContext, R.string.alert_empty_post); return; }
+
+        try {
+            // call service intro check
+            // String 방식
+//            StringBuilder fullParams = new StringBuilder("{");
+//            fullParams.append("\"UUID\":\""+userVo.getUuID()+"\"")
+//                    .append(",\"UUPN\":\""+userVo.getUuPN()+"\"")
+//                    .append(",\"UUOS\":\""+userVo.getUuOs()+"\"")
+//                    .append(",\"UUDEVICE\":\""+userVo.getUuDevice()+"\"")
+//                    .append(",\"UUTOKEN\":\""+userVo.getUuToken()+"\"")
+//                    .append(",\"UUAPP\":\""+userVo.getUuAppVer()+"\"")
+//                    .append("}");
+            // Map 방식 0
+            Map<String, String> params = new HashMap<String, String>();
+            //params.put("UUID", MHDApplication.getInstance().getMHDSvcManager().getDeviceNewUuid());
+            params.put("UUPN", Util.getInstance().getPhoneNumber(mContext));
+            params.put("UUOS", Integer.toString(Build.VERSION.SDK_INT));
+            params.put("UUDEVICE", Build.MODEL);
+            //params.put("UUTOKEN", MHDApplication.getInstance().getMHDSvcManager().getFcmToken());
+            params.put("UUAPP", MHDApplication.getInstance().getAppVersion());
+            params.put("UUPOST", editor.getContentAsHTML());
+            MHDNetworkInvoker.getInstance().sendVolleyRequest(mContext, R.string.url_restapi_regist_member, params, ((MainActivity)getActivity()).responseListener);
+
+//            new Handler().postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
+//                    Intent intent = new Intent(mContext, MainActivity.class);
+//                    intent.putExtra("field", "value");
+//                    startActivity(intent);
+//                    overridePendingTransition(0, 0);
+//                    finish();
+//                }
+//            }, 500);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            MHDLog.printException(e);
+        }
+        //Main 이동
     }
 }
