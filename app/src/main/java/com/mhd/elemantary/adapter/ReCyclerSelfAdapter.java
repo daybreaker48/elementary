8610291565cfc31c85fb22e02ca6fbc50d7b310b
daybreaker48 <prof.dagian@gmail.com>
2021-06-24 16:43:48 +0900

    스스로해요 화면 구성 중(자리 이동)

---
+++ app/src/main/java/com/mhd/elemantary/adapter/ReCyclerSelfAdapter.java
@@ -0,0 +1,67 @@
+package com.mhd.elemantary.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.mhd.elemantary.R;
+import com.mhd.elemantary.common.vo.SelfData;
+import com.mhd.elemantary.common.vo.TodoData;
+import com.mhd.elemantary.util.MHDLog;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ReCyclerSelfAdapter extends RecyclerView.Adapter<ReCyclerSelfAdapter.RecyclerViewHolder> {
+
+    private ArrayList<SelfData> listData = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View holderView = LayoutInflater.from(parent.getContext()).inflate(R.layout.self_list_holder_view, parent, false);
+        RecyclerViewHolder recyclerViewHolder = new RecyclerViewHolder(holderView);
+        MHDLog.d("dagian", "viewholder create");
+        return recyclerViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerViewHolder holder, int position) {
+        // Item을 하나, 하나 보여주는(bind 되는) 함수입니다.
+        holder.onBind(listData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        // RecyclerView의 총 개수 입니다.
+        return listData.size();
+    }
+
+    public void addItem(SelfData data) {
+        // 외부에서 item을 추가시킬 함수입니다.
+        listData.add(data);
+    }
+
+    public static class RecyclerViewHolder extends RecyclerView.ViewHolder{
+        public TextView tvSelfItem;
+        public CheckBox cb_self_complete_holder;
+//        public ImageView imageView;
+
+        public RecyclerViewHolder(View view){
+            super(view);
+
+            this.tvSelfItem = view.findViewById(R.id.tv_self_item_holder);
+            this.cb_self_complete_holder = view.findViewById(R.id.cb_self_complete_holder);
+        }
+
+        void onBind(SelfData data) {
+//            holder.tvSubject.setText(this.textSet1[position]);
+//        holder.imageView.setBackgroundResource(this.imgSet[position]);
+            this.tvSelfItem.setText(data.getSelfItem());
+        }
+    }
+}
--- app/src/main/java/com/mhd/elemantary/fragment/SelfFragment.java
+++ app/src/main/java/com/mhd/elemantary/fragment/SelfFragment.java
@@ -8,13 +8,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.mhd.elemantary.R;
-import com.mhd.elemantary.adapter.ReCyclerAdapter;
+import com.mhd.elemantary.adapter.ReCyclerSelfAdapter;
+import com.mhd.elemantary.common.vo.SelfData;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class SelfFragment extends BaseFragment {
-
     private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
+    private ReCyclerSelfAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
 
     public static SelfFragment create() {
@@ -36,9 +39,12 @@ public class SelfFragment extends BaseFragment {
 
         RecyclerView recyclerView = (RecyclerView) root.findViewById(R.id.recv_receiving);
         recyclerView.setHasFixedSize(true);
-
         layoutManager = new LinearLayoutManager(mContext);
         recyclerView.setLayoutManager(layoutManager);
+        adapter = new ReCyclerSelfAdapter();
+        recyclerView.setAdapter(adapter);
+
+        getData();
     }
 
     @Override
@@ -47,5 +53,23 @@ public class SelfFragment extends BaseFragment {
 //            // editor 내용 초기화.
 //            editor.clearAllContents();
 //        }
+    }
+
+
+    private void getData() {
+        // 임의의 데이터입니다.
+        List<String> listSelfItem = Arrays.asList("집안일 돕기", "스스로 씻기", "치카", "화분물주기");
+
+        for (int i = 0; i < listSelfItem.size(); i++) {
+            // 각 List의 값들을 data 객체에 set 해줍니다.
+            SelfData data = new SelfData();
+            data.setSelfItem(listSelfItem.get(i));
+
+            // 각 값이 들어간 data를 adapter에 추가합니다.
+            adapter.addItem(data);
+        }
+
+        // adapter의 값이 변경되었다는 것을 알려줍니다.
+        adapter.notifyDataSetChanged();
     }
 }
