c3ac192b91f2d2e27bffb535c46af4690ab33746
daybreaker48 <prof.dagian@gmail.com>
2021-07-05 16:56:54 +0900

    회원 가입 작업 중(자리 이동)

--- app/src/main/java/com/mhd/elemantary/activity/JoinActivity.java
+++ app/src/main/java/com/mhd/elemantary/activity/JoinActivity.java
@@ -1,6 +1,7 @@
 package com.mhd.elemantary.activity;
 
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.ColorDrawable;
@@ -10,11 +11,15 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.mhd.elemantary.R;
 import com.mhd.elemantary.common.MHDApplication;
+import com.mhd.elemantary.common.vo.UserVo;
 import com.mhd.elemantary.constant.MHDConstants;
 import com.mhd.elemantary.network.MHDNetworkInvoker;
 import com.mhd.elemantary.util.MHDDialogUtil;
@@ -29,8 +34,9 @@ import java.util.Map;
 public class JoinActivity extends BaseActivity {
 
     private CheckBox btnCheckTerms, btnCheckPrivacy, btnCheckGps;
-    private int selectedYear = 0;
     private TextView btnTerms, btnPrivacy, btnGps;
+    private EditText et_join_name, et_join_id, et_join_pwd;
+    private Button btn_join;
     private String selectedSex = "M";
 
     @Override
@@ -44,6 +50,10 @@ public class JoinActivity extends BaseActivity {
         btnTerms = (TextView) findViewById(R.id.tv_terms_all_view);
         btnPrivacy = (TextView) findViewById(R.id.tv_privacy_all_view);
         btnGps = (TextView) findViewById(R.id.tv_gps_all_view);
+        btn_join = (Button) findViewById(R.id.btn_join);
+        et_join_name = (EditText) findViewById(R.id.et_join_name);
+        et_join_id = (EditText) findViewById(R.id.et_join_id);
+        et_join_pwd = (EditText) findViewById(R.id.et_join_pwd);
 
         btnTerms.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,6 +73,12 @@ public class JoinActivity extends BaseActivity {
                 showTerms("G");
             }
         });
+        btn_join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                joinService();
+            }
+        });
     }
 
     private void showPicker(){
@@ -74,7 +90,6 @@ public class JoinActivity extends BaseActivity {
         yearDialog.setContentView(R.layout.picker_year);
 
         final NumberPicker numberPicker = (NumberPicker) yearDialog.findViewById(R.id.np_year_picker);
-        final Button confirmBtn = (Button) yearDialog.findViewById(R.id.bt_confirm);
         numberPicker.setMinValue(year - 100);
         numberPicker.setMaxValue(year - 15);
         numberPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
@@ -84,19 +99,9 @@ public class JoinActivity extends BaseActivity {
         numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-
             }
         });
 
-//        confirmBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                selectedYear = numberPicker.getValue();
-//                btnYear.setText(String.valueOf(selectedYear + " 년"));
-//
-//                yearDialog.dismiss();
-//            }
-//        });
         yearDialog.show();
     }
 
@@ -132,10 +137,12 @@ public class JoinActivity extends BaseActivity {
         //입력된 정보를 취합해서 서버 전송
 
         //필터링. (gender, year, agree terms)
-        if(selectedYear == 0){ MHDDialogUtil.sAlert(mContext, R.string.alert_not_select_year); return; }
         if(!btnCheckTerms.isChecked()){ MHDDialogUtil.sAlert(mContext, R.string.alert_not_agree_terms); return; }
         if(!btnCheckPrivacy.isChecked()){ MHDDialogUtil.sAlert(mContext, R.string.alert_not_agree_privacy); return; }
         if(!btnCheckGps.isChecked()){ MHDDialogUtil.sAlert(mContext, R.string.alert_not_agree_gps); return; }
+        if(et_join_name.getText().toString().length() == 0) { MHDDialogUtil.sAlert(mContext, R.string.alert_not_name); return; }
+        if(et_join_id.getText().toString().length() == 0) { MHDDialogUtil.sAlert(mContext, R.string.alert_not_id); return; }
+        if(et_join_pwd.getText().toString().length() == 0) { MHDDialogUtil.sAlert(mContext, R.string.alert_not_pwd); return; }
 
         try {
             // call service intro check
@@ -156,8 +163,9 @@ public class JoinActivity extends BaseActivity {
             params.put("UUDEVICE", Build.MODEL);
             //params.put("UUTOKEN", MHDApplication.getInstance().getMHDSvcManager().getFcmToken());
             params.put("UUAPP", MHDApplication.getInstance().getAppVersion());
-            params.put("UUSEX", selectedSex);
-            params.put("UUYEAR", Integer.toString(selectedYear));
+            params.put("UUNAME", et_join_name.getText().toString());
+            params.put("UUID", et_join_id.getText().toString());
+            params.put("UUPWD", et_join_pwd.getText().toString());
             MHDNetworkInvoker.getInstance().sendVolleyRequest(mContext, R.string.url_restapi_regist_member, params, responseListener);
 
 //            new Handler().postDelayed(new Runnable() {
@@ -175,5 +183,50 @@ public class JoinActivity extends BaseActivity {
             MHDLog.printException(e);
         }
         //Main 이동
+    }
+
+    @Override
+    protected boolean networkResponseProcess(String result) {
+        boolean resultFlag = super.networkResponseProcess(result);
+        MHDLog.d(TAG, "networkResponseProcess resultFlag >>> " + resultFlag);
+
+        if(!resultFlag) return resultFlag;
+
+        // resultFlag 이 true 라면 현재 여기에 필요한 data 들이 전역에 들어가 있는 상태.
+
+        if("M".equals(nvResultCode)){
+            // Just show nvMsg
+            MHDDialogUtil.sAlert(mContext, nvMsg);
+        }else if("S".equals(nvResultCode)){
+            // 회원가입 성공. user vo 를 구성하고
+            MHDLog.d(TAG, "networkResponseProcess nvMsg >>> " + nvMsg);
+
+            // 기존 사용자인 경우.
+            // vo 및 각종 변수에 저장하고 메인으로 넘긴다. 레코드가 하나여도 jsonarray 로 보내니 gson에서 에러가 나드라.
+            Gson gson = new Gson();
+            UserVo userVo;
+            userVo = gson.fromJson(nvMsg, UserVo.class);
+            MHDApplication.getInstance().getMHDSvcManager().setUserVo(null);
+            MHDApplication.getInstance().getMHDSvcManager().setUserVo(userVo);
+
+            if(MHDApplication.getInstance().getMHDSvcManager().getUserVo() != null){
+                // MainActivity 로 이동
+                goMain();
+            }
+            // 학생을 등록하겠느냐는 컨펌창을 띄우고 확인 -> 등록창, 취소 -> 메인
+            MHDDialogUtil.sAlert(this, R.string.alert_join_success, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // 학생 등록창으로 이동.
+                }
+            }, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // 메인으로 이동. 아무 내용 안나올 것.
+                }
+            });
+        }
+
+        return true;
     }
 }
